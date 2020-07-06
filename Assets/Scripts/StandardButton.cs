e642014d613a824ebe4b5ccc6d7779d8132784cc
ryo <akstkr@gmail.com>
2020-07-07 00:08:23 +0900

    add: 修正

--- Assets/Scripts/StandardButton.cs
+++ Assets/Scripts/StandardButton.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using UniRx;
 
 public class StandardButton : MonoBehaviour
 {
-	[SerializeField] private Button button;
+	[SerializeField] 
+	private Button button;
 
+	private ButtonAction onClickCallBack = null;
 
 	private void Awake() {
-		button.onClick.AsObservable().Subscribe(_ => Debug.Log("OnClickAction"));
+		button.onClick.AsObservable().Subscribe(_ => {
+			if(onClickCallBack != null)onClickCallBack.onClick();
+//			Debug.Log("OnClickAction");
+			});
 	}
+
+	public void SetButtonAction(ButtonAction _onClickCallBack) {
+		this.onClickCallBack = _onClickCallBack;
+	}
+
+}
+
+public abstract class ButtonAction{
+	public void onClick() { }
 }
